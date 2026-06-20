96448b4cdfa8ae0f62a5957aee883d900ccd84ab
Zain Niaz <F23-0216@student.uoh.edu.pk>
2026-06-20 08:56:57 +0000

    Added DockerFile Support

--- backend_api/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Exit on error
-set -o errexit
-
-# Install Python dependencies
-pip install -r requirements.txt
-
-# Install Playwright browser (Chromium only to save space)
-playwright install chromium
